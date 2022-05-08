062a4956500f8ffedd4953e7349077801a793a9a
jasmin.nguyen <jasmin.nguyen@bulme.at>
2022-05-08 19:56:11 +0200

    Verbesserung

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
+
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLCDNumber, QCheckBox, QSlider
 from PyQt5.QtCore import Qt, QSize, QTimer
-from gpiozero import LEDBoard
+from gpiozero import LED, LEDBoard
+from signal import pause
 
-LED0= 18
-LED1= 23
-LED2= 24
-LED3= 25
-
-leds = LEDBoard(LED0, LED1, LED2, LED3)
 
 # Klasse für das Hauptfenster
 class MyWindow(QMainWindow):
@@ -17,63 +13,71 @@ class MyWindow(QMainWindow):
 
         # Konstruktor von QMainWindow aufrufen
         super().__init__()
-        self.setMinimumSize(QSize(300, 120))    
+        self.leds=LEDBoard(18,23,24,25)
+        self.setMinimumSize(QSize(700, 500))    
         self.setWindowTitle('Bit Konverter') 
 
-        wid = QWidget(self)
+        wid = QtWidgets.QWidget(self)
         self.setCentralWidget(wid)
         
-        vlayout = QVBoxLayout()
-        wid.setLayout(vlayout)
+        vbox = QtWidgets.QVBoxLayout()
+        wid.setLayout(vbox)
 
         
         # Slider + Label Anzeige Dezimalwert
         
-        self.slider = QSlider(Qt.Horizontal)
+        self.slider = QSlider(Qt.Horizontal,wid)
         self.slider.setMinimum(0)
         self.slider.setMaximum(15)
-        self.slider.setTickPosition(QSlider.TicksBelow)
-        self.slider.setSingleStep(1)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.valueChanged[int].connect(self.slidercount)
+        self.label = QLabel('0')
         
-        self.slider.connect(self.slidercount,self.my_on)
-            
-        self.label = QLabel()
-        self.slider.valueChanged.connect(self.label.setNum)
              
         sliderbox = QHBoxLayout()
             
         sliderbox.addWidget(self.slider)
         sliderbox.addWidget(self.label)
+        vbox.addLayout(sliderbox)
 
         # Labels fuer 4 Bits
         self.bitlabels = [QLabel('8'),QLabel('4'),QLabel('2'),QLabel('1')] # Liste 
-        self.bitlabels.setStyleSheet("background-color:rgb(170,170,170")
         
         # hier bitlabels erstellen      
         bitbox = QHBoxLayout()
-        for bitlabel in self.bitlabels:
-            bitbox.addWidget(bitlabels)
-
+        
+        
         # Layout zusammenbauen
-        vbox = QVBoxLayout()
-        vbox.addLayout(sliderbox)
-        vbox.addLayout(bitbox)
+        vboxBit= QVBoxLayout()
+        vboxBit.addLayout(bitbox)
+        vbox.addLayout(vboxBit)
+        
+        for index,bitlabel in enumerate(self.bitlabels):
+            bitbox.addWidget(bitlabel)
+            bitlabel.setStyleSheet("background-color:rgb(140,140,140)")
+            bitlabel.setFixedWidth(20)
+            bitlabel.setFixedHeight(20)
+            bitlabel.setAlignment(Qt.AlignCenter)
 
-        # vbox anzeigen in QWidget
-        self.setLayout(vbox)
-
-    
     def my_on(self):
         for index in self.bitlabels:
             self.bitlabels=leds[index].on()
             
             
-    def slidercount(self):
-        if self.slider.sliderPressed():
-            for lb in self.bitlabels:
-                while lb != self.slider.value():
-                    lb+=lb
-                    QLabel.setStyleSheet("background-color:rgb(255,0,0)")
+    def slidercount(self,value):
+        showValue=str(value)
+        self.label.setText(showValue)
+        onColor= "background-color: rgb(255,0,0)"
+        offColor= "background-color: rgb(140,140,140)"
+        for i in range(4):
+            div= 2**(3-i)
+            checkValue= int(value/div)
+            if checkValue %2 ==1:
+                self.bitlabels[i].setStyleSheet(onColor)
+                self.leds[i].on()
+            else:
+                self.bitlabels[i].setStyleSheet(offColor)
+                self.leds[i].off()
                 
         
 app = QtWidgets.QApplication([])
